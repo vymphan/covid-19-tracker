70df317a53fcd3763faf2cf87eccfa22a2821d6b
Vy Phan <nalu.phan@gmail.com>
2022-03-14 21:06:42 -0400

    Updates

---
+++ precache-manifest.019def708bb3ce880f54485e7a6b1825.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8323c3c4ce7fd7908e2eb4ba940856e7",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "1fac19a81dda05cb7de5",
+    "url": "/covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "22cd6b4eb895a7645823",
+    "url": "/covid-19-tracker/static/css/main.862f3173.chunk.css"
+  },
+  {
+    "revision": "1fac19a81dda05cb7de5",
+    "url": "/covid-19-tracker/static/js/2.b9398e36.chunk.js"
+  },
+  {
+    "revision": "ff1ddb6ed9fcda773ff64221985797b2",
+    "url": "/covid-19-tracker/static/js/2.b9398e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cd6b4eb895a7645823",
+    "url": "/covid-19-tracker/static/js/main.1946aadf.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  },
+  {
+    "revision": "74f7cdffc9b2c3f62771c7acac3f512d",
+    "url": "/covid-19-tracker/static/media/BG1.74f7cdff.png"
+  },
+  {
+    "revision": "0f8715707fdd1c3eb4b5335373a9a726",
+    "url": "/covid-19-tracker/static/media/virus-enhanced.0f871570.png"
+  }
+]);
